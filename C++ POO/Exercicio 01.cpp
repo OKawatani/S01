6d201e31e9c98927949d1a9d61cc13d68a1c945d
OKawatani <eduardokawatani@gmail.com>
2025-10-02 18:50:09 -0300

    Novos arquivos

---
+++ C++ POO/Exercicio 01.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+class Personagem 
+{
+private:
+    /* data */
+public:
+    string nome;
+    int nivel;
+    int dano;
+    int vida;
+
+    Personagem(string nome, int nivel, int dano, int vida)
+    {
+        this->nome = nome;
+        this->nivel = nivel;
+        this->vida = vida;
+        this->dano = dano;
+    }
+
+    void atacar(Personagem p0)
+    {
+        p0.vida -= this->dano;
+        cout << p0.nome << " Perdeu " << this->dano << " de vida" << endl;
+    }
+
+
+};
+
+
+
+int main()
+{
+    Personagem p1("Jorge", 3, 1, 3);
+    Personagem p2("Ronaldo", 3, 1, 5);
+
+    p1.atacar(p2);
+
+    return 0;
+}
