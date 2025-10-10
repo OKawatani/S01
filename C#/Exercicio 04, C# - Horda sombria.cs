bad5d967f18d28d6ca4eedcca5b65f240d28d974
OKawatani <eduardokawatani@gmail.com>
2025-10-09 21:27:37 -0300

    Update Exercicio 04, C# - Horda sombria.cs

--- C#/Exercicio 04, C# - Horda sombria.cs
+++ C#/Exercicio 04, C# - Horda sombria.cs
@@ -1,1 +1,58 @@
+using System;
+
+abstract class MonstroSombrio
+{
+    public string Nome { get; set; }
+
+    public MonstroSombrio(string nome)
+    {
+        Nome = nome;
+    }
+
+    public virtual void Mover()
+    {
+        Console.WriteLine($"{Nome} se move de forma misteriosa...");
+    }
+}
+
+class Zumbi : MonstroSombrio
+{
+    public Zumbi(string nome) : base(nome) { }
+
+    
+    public override void Mover()
+    {
+        Console.WriteLine($"{Nome} (Zumbi) anda lentamente, arrastando os pés...");
+    }
+}
+
+class Espectro : MonstroSombrio
+{
+    public Espectro(string nome) : base(nome) { }
+
+
+    public override void Mover()
+    {
+        Console.WriteLine($"{Nome} (Espectro) desliza rapidamente pelo ar, quase invisível!");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+        MonstroSombrio[] horda = new MonstroSombrio[]
+        {
+            new Zumbi("Zumbi Lento"),
+            new Espectro("Espectro Sombrio")
+        };
+
+        
+        foreach (MonstroSombrio monstro in horda)
+        {
+            monstro.Mover();
+        }
+    }
+}
 
