c0ea5319e05d99ed26846dce892a78e43d9d1e52
OKawatani <eduardokawatani@gmail.com>
2025-11-27 21:01:07 -0300

    Create Exercicio 04, Prolog - Ancestralidade.pl

---
+++ Prolog/Exercicio 04, Prolog - Ancestralidade.pl
@@ -0,0 +1,17 @@
+progenitor(urano, cronos).
+progenitor(gaia, cronos).
+
+progenitor(cronos, zeus).
+progenitor(cronos, poseidon).
+progenitor(cronos, hades).
+
+progenitor(zeus, atena).
+progenitor(zeus, ares).
+
+ancestral(A, D) :-
+    progenitor(A, D).
+
+
+ancestral(A, D) :-
+    progenitor(A, Z),
+    ancestral(Z, D).
